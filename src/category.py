c0b1d25b5384fde0ce502d1e366610286c958be6
unknown <sanya@example.com>
2025-01-26 11:47:35 +0700

    add_3

--- src/category.py
+++ src/category.py
@@ -1,14 +1,54 @@
+from src.product import Product
+
+
 class Category:
-    ''' Класс, описывающий категории '''
-    name: str
-    description: str
-    products: list
-    number_categories = 0
-    number_products = 0
+    """Класс, описывающий категории"""
+
+    name: str  # название
+    description: str  # описание
+    products: list  # список товаров категории
+    number_categories = 0  # количество категорий
+    number_products = 0  # количество товаров
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.number_categories += 1
         Category.number_products += len(products)
+
+    def __str__(self):
+        return f"{self.name}, количество продуктов: {Category.number_products} шт."
+
+    def add_product(self, new_products: Product):
+        if isinstance(new_products, Product):
+            self.__products.append(new_products)
+            Category.number_products += 1
+        else:
+            raise TypeError("Нельзя добавлять разные классы")
+
+    @property
+    def products(self):
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return product_str
+
+
+# if __name__ == "__main__":
+#     category1 = Category("Одежда", "Виды одежды", [])
+#
+#     product1 = Product("Футболка", "Футболка размера-Х", 500, 10)
+#     product2 = Product("Джинсы", "Джинсы синие", 1000, 5)
+#     category1.add_product(product1)
+#     category1.add_product(product2)
+#     print(category1)
+#     print(category1.name)
+#     print(category1.description)
+#     print(category1.products)
+#     print(category1.number_categories)
+#     print(category1.number_products)
+#
+#     category2 = Category("Продукты", "Мясные изделия", ["Колбаса", "Сосиски", "Сало"])
+#     print(category2.number_categories)
+#     print(category2.number_products)
--- src/product.py
+++ src/product.py
@@ -1,12 +1,102 @@
 class Product:
-    ''' Класс, описывающий продукты '''
-    name: str
-    description: str
-    price: float
-    quantity: int
+    """Класс, описывающий продукты"""
+
+    name: str  # название
+    description: str  # описание
+    price: float  # цена
+    quantity: int  # количество в наличии
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    def __str__(self):
+        return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        if type(other) is Product:
+            sum_goods = (self.__price * self.quantity) + (other.__price * other.quantity)
+            return sum_goods
+        else:
+            raise TypeError
+
+    @classmethod
+    def new_product(cls, product):
+        name = product["name"]
+        description = product["description"]
+        price = product["price"]
+        quantity = product["quantity"]
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
+
+
+class Smartphone(Product):
+    """Класс, Смартфон"""
+
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency  # производительность
+        self.model = model  # модель
+        self.memory = memory  # объем встроенной памяти
+        self.color = color  # цвет
+
+    def __add__(self, other):
+        if type(other) is Smartphone:
+            sum_goods = (self.price * self.quantity) + (other.price * other.quantity)
+            return sum_goods
+        else:
+            raise TypeError
+
+
+class LawnGrass(Product):
+    """Класс, Трава газонная"""
+
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country  # страна-производитель
+        self.germination_period = germination_period  # срок прорастания
+        self.color = color  # цвет
+
+    def __add__(self, other):
+        if type(other) is LawnGrass:
+            sum_goods = (self.price * self.quantity) + (other.price * other.quantity)
+            return sum_goods
+        else:
+            raise TypeError
+
+
+# if __name__ == "__main__":
+#     product1 = Smartphone("Телефоны", "Cмартфоны", 97700, 7, "Apple", "iPhone 14 Pro Max", "256 ГБ", "Золотой")
+#     product2 = Smartphone("Телефоны", "Cмартфоны", 127700, 12, "Apple", "iPhone 15 Pro Max", "256 ГБ", "Золотой")
+#     print(product1.name)
+#     print(product1.description)
+#     print(product1.price)
+#     print(product1.quantity)
+#     print(product1.efficiency)
+#     print(product1.model)
+#     print(product1.memory)
+#     print(product1.color)
+#     print(product1+product2)
+#
+#     product3 = LawnGrass("Покрытие для спорта", "Для футбола", 156000, 55, "Россия", "5 суток", "Зеленая")
+#     product4 = LawnGrass("Покрытие для спорта", "Для футбола", 106500, 74, "Россия", "5 суток", "Зеленая")
+#     print(product3.name)
+#     print(product3.description)
+#     print(product3.price)
+#     print(product3.quantity)
+#     print(product3.country)
+#     print(product3.germination_period)
+#     print(product3.color)
+#     print(product3 + product4)
